df69305b1f32ef237f3087e62cf2f311b1cf7b48
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-05-23 00:09:22 +0800

    Update and rename task1.c to test1.c
    格式游戏

--- tests/test1.c
+++ tests/test1.c
@@ -1,9 +1,18 @@
-int a[1000];
+int a[100000];
 int n;
 int pivot; 
 int t;
 int I;
 int tr;
+
+int isSorted(int x,int y,int z,int w){
+    tr = 1;
+    for(z=0;z<y-1;z=z+1){
+        if(a[x+z]>a[x+z+1])tr=0;
+        if(tr==0)z=y;
+    }
+    return tr;
+}
 
 int qsort(int st, int arrN, int i, int j){
     pivot = a[st];
@@ -27,10 +36,12 @@ int qsort(int st, int arrN, int i, int j){
         a[st+i] = a[st+j];
         a[st+j] = t;
     }
-    if(i>1)qsort(st, i, 0, i-1);
-    if(arrN-i-1>1)qsort(st+i+1, arrN-i-1, 0, arrN-i-2);
+    if(isSorted(st,arrN,i,j)==0){
+        if(i>1)qsort(st, i, 0, i-1);
+        if(arrN-i-1>1)qsort(st+i+1, arrN-i-1, 0, arrN-i-2);
+    }
     return 0;
-}
+}   
 
 int main(){
     read(n,int);
@@ -38,12 +49,12 @@ int main(){
     for(I=0;I<n;I=I+1){
         read(a[I],int);
     }
-    qsort(0,n,0,n-1);
+    if(n>1)qsort(0,n,0,n-1);
     I = 0;
     for(I=0;I<n;I=I+1){
         write(a[I],int);
-        write(" ",int);
+        write("\n",char);
     }
-    write("\n",int);
+    //if(I==0)write("\n",char);
     return 0;
 }
